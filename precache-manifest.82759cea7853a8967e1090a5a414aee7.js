700bdafa4cbbd2500da192a045417d934db4ea64
yavgen999 <57105830+yavgen999@users.noreply.github.com>
2019-10-28 19:19:26 +0300

    Add files via upload

---
+++ precache-manifest.82759cea7853a8967e1090a5a414aee7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkapps/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "88e0734f1967e51210a6",
+    "url": "/vkapps/static/js/runtime~main.88e0734f.js"
+  },
+  {
+    "revision": "750e5f014b0a5ecb38a3",
+    "url": "/vkapps/static/js/main.750e5f01.chunk.js"
+  },
+  {
+    "revision": "b24a9e4e46d677c6c458",
+    "url": "/vkapps/static/js/2.b24a9e4e.chunk.js"
+  },
+  {
+    "revision": "750e5f014b0a5ecb38a3",
+    "url": "/vkapps/static/css/main.6704a9a5.chunk.css"
+  },
+  {
+    "revision": "b24a9e4e46d677c6c458",
+    "url": "/vkapps/static/css/2.abc87da9.chunk.css"
+  },
+  {
+    "revision": "bfe6b95be5784ec4422bf008454c2ef1",
+    "url": "/vkapps/index.html"
+  }
+];
